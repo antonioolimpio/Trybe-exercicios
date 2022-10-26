68b44457f09529502f4803df8070fa235bb712f9
Antonio Olimpio <jnbarroos1@gmail.com>
2022-10-26 20:50:46 -0300

    exercicios

---
+++ Fundamentos/Seção-2-Introdução.JavaScript/Dia-4-Objetos.Funções/Exercício.js
@@ -0,0 +1,108 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+ 
+ };
+
+   console.log('Bem Vinda, '+ info.personagem);
+   ------ 1 -------
+
+   let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+ };
+ 
+   console.log(info);
+   ------ 2 -------
+
+   let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+ };
+ 
+   for(let key in info){
+    console.log(key);
+}
+   ------ 3 ------
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+ };
+ 
+   for(let key in info){
+    console.log(info[key]);
+}
+ ------ 4 -----
+
+ let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+  
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+  
+  for (let properties in info) {
+    if (
+      info[properties] === 'Sim' &&
+      info2[properties] === 'Sim'
+    ) {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(info[properties] + ' e ' + info2[properties]);
+    }
+  }
+  ------ 5 ------
+//O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+  
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0]['titulo']);
+  ----- 6 ------
+
+  let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+  
+  leitor.livrosFavoritos[1] = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+
+  console.log(leitor.livrosFavoritos);
+  ----- FIM ------
